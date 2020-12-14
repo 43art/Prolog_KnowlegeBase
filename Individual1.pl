b29be1097ce41c9c19bc05eb6dc74f24b348bb20
Alexey Zuev <73071789+43art@users.noreply.github.com>
2020-12-14 13:13:10 +0300

    Create answer and loopA

--- Individual1.pl
+++ Individual1.pl
@@ -9,9 +9,21 @@ pr(N,I):-Ost is N mod I, Ost > 0, I1 is I+1, pr(N,I1).
 maxB(TempRes, Ans, MaxB, _, Res, ResB):-TempRes >= Ans, Res is TempRes, ResB is MaxB,!.
 maxB(_, Ans, _, B, Res, ResB):-Res is Ans, ResB is B.
 
-loopN(A, B, N, Ans):-polynomial(A, B, N, Res), pr(Res),
-    N1 is N + 1, loopN(A, B, N1, Ans); Ans is N,!.
+maxA(TempRes, TempResA, MaxA, _, MaxB, _, Res, ResA, ResB):-TempRes >= TempResA,
+    Res is TempRes, ResA is MaxA, ResB is MaxB,!.
+maxA(_, TempResA, _, A, _, B, Res, ResA, ResB):-Res is TempResA, ResA is A, ResB is B.
+
+loopN(A, B, N, AnsN):-polynomial(A, B, N, Res), pr(Res),
+    N1 is N + 1, loopN(A, B, N1, AnsN); AnsN is N,!.
 
 loopB(_, B, TempRes, MaxB, Res, ResB):-B > 1000, Res is TempRes, ResB is MaxB,!.
-loopB(A, B, TempRes, MaxB, Res, ResB):-loopN(A, B, 0, Ans),
-    maxB(TempRes, Ans, MaxB, B, Temp, TempB), B1 is B + 1, loopB(A, B1, Temp, TempB, Res, ResB).
+loopB(A, B, TempRes, MaxB, Res, ResB):-loopN(A, B, 0, AnsN),
+    maxB(TempRes, AnsN, MaxB, B, Temp, TempB), B1 is B + 1, loopB(A, B1, Temp, TempB, Res, ResB).
+
+loopA(A, TempRes, MaxA, MaxB, Res, ResA, ResB):-A > 1000, Res is TempRes, ResA is MaxA, ResB is MaxB,!.
+loopA(A, TempRes, MaxA, MaxB, Res, ResA, ResB):-
+    loopB(A, -1000, 0, 0, TempResA, TempMaxB), maxA(TempRes, TempResA, MaxA, A, MaxB, TempMaxB, ResMax, TempA, TempB),
+    A1 is A + 1, loopA(A1, ResMax, TempA, TempB, Res, ResA, ResB).
+
+answer:-loopA(-1000, 0, 0, 0, Res, ResA, ResB), Answer is ResA * ResB,
+    write(Res), write(' '), write(Answer), write(' '), write(ResA), write(' '), write(ResB),!.
