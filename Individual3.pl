36f8fe600d749f13470f84bcd23676f84dc1cc47
Alexey Zuev <73071789+43art@users.noreply.github.com>
2020-12-18 23:14:25 +0300

    Final Individual3

--- Individual3.pl
+++ Individual3.pl
@@ -9,9 +9,22 @@ readStrStatus(10):-readStrFile(Flag), readStrStatus(Flag).
 readStrFile(Flag):-get0(X), readStr(X, [], Flag).
 
 readStr(10, A, 10):-writeStr(A), nl,!.
-readStr(37, A, Flag):-get0(X), skipOneLine(X, A, Flag),!.
+readStr(37, A, Flag):-get0(X1), skipOneLine(X1, A, Flag),!.
+readStr(47, A, Flag):-get0(X1), leftSide(X1, A, Flag),!.
 readStr(-1, A, -1):-writeStr(A), seen, told,!.
 readStr(X, A, Flag):-append(A, [X], A1), get0(X1), readStr(X1, A1, Flag). 
+
+leftSide(42, A, Flag):-get0(X1), skipMultiline(X1, A, Flag).
+leftSide(-1, A, -1):-readStr(-1, A, -1).
+leftSide(B, A, Flag):-append(A, [47], A1), append(A1, [B], B1), get0(X1), readStr(X1, B1, Flag).
+
+rightSide(47, A, Flag):-get0(X1), readStr(X1, A, Flag),!.
+rightSide(B, A, _):-skipMultiline(B, A, _).
+rightSide(10, A, _):-get0(X1), rightSide(X1, A, _).
+rightSide(-1, A, -1):-readStr(-1, A, -1),!.
+
+skipMultiline(42, A, Flag):-get0(X1), rightSide(X1, A, Flag).
+skipMultiline(_, A, _):-get0(X1), skipMultiline(X1, A, _).
 
 skipOneLine(10, A, 10):-writeStr(A), nl,!.
 skipOneLine(_, A, _):-get0(X), skipOneLine(X, A, _).
